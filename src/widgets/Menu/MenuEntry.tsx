eefba992d38ad49f732ca6cbe6e5935c66fa3023
Alejandro Camps Guerrero <kaatalhama@gmail.com>
2021-06-20 16:47:47 +0200

    add target blank to menu link and change colors

--- src/widgets/Menu/MenuEntry.tsx
+++ src/widgets/Menu/MenuEntry.tsx
@@ -33,7 +33,8 @@ const MenuEntry = styled.div<Props>`
   background-color: ${({ secondary, theme }) => (secondary ? theme.colors.background : "transparent")};
   color: ${({ theme }) => theme.colors.textSubtle};
   box-shadow: ${({ isActive, theme }) => (isActive ? `inset 4px 0px 0px ${theme.colors.primary}` : "none")};
-
+  font-family: 'Orbitron', 'Kanit', sans-serif;
+  
   a {
     display: flex;
     align-items: center;
